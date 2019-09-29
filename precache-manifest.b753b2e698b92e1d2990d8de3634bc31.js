388857b415ed59625536a0b9ed0940ad5ad7dc31
Katherine Sutarlim <katherine.rnslim.sutarlim@gmail.com>
2019-09-29 13:27:49 +1000

    Updates

---
+++ precache-manifest.b753b2e698b92e1d2990d8de3634bc31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7278947502763bc8d74c62834c5f317",
+    "url": "/assessment-react-web-dev/index.html"
+  },
+  {
+    "revision": "abb244395d2e435eab3d",
+    "url": "/assessment-react-web-dev/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "8f394a04dbc49833565b",
+    "url": "/assessment-react-web-dev/static/js/2.eecdd24e.chunk.js"
+  },
+  {
+    "revision": "abb244395d2e435eab3d",
+    "url": "/assessment-react-web-dev/static/js/main.6e0dfb4e.chunk.js"
+  },
+  {
+    "revision": "ec932b9a23c7b22dbd87",
+    "url": "/assessment-react-web-dev/static/js/runtime-main.819da9c3.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/assessment-react-web-dev/static/media/logo.25bf045c.svg"
+  }
+]);
